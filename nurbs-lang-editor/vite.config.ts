42f7a9c04a096a99d239e84a61d9f4ff542cba47
Quinn-Cattanach <qcattanach@gmail.com>
2025-12-01 14:36:10 -0500

    gh workflow

--- nurbs-lang-editor/vite.config.ts
+++ nurbs-lang-editor/vite.config.ts
@@ -1,18 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import fs from "fs";
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        https: {
-            key: fs.readFileSync("localhost+2-key.pem"),
-            cert: fs.readFileSync("localhost+2.pem"),
-        },
-        host: true,
-    },
+    base: "./", // VERY IMPORTANT for GH Pages + WASM
     build: {
-        outDir: "../docs",
-        emptyOutDir: true,
+        target: "esnext",
     },
 });
